a9ef7e63dfdb9987743ac3ea882e602de497f5bd
Ivan Gomes <ivanlourencogomes@gmail.com>
2024-02-20 17:01:30 +0000

    mobile menu structure and JS functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('main script');
+
+
+window.addEventListener("load", (event) => {
+
+    let menu = document.querySelector("header nav ul.menu");
+    let menuIcon = document.querySelector("header nav .menu-icon");
+    let closeMenu = document.querySelector("header nav .close-icon");
+
+    menuIcon.addEventListener("click", () => {
+        menu.classList.add("open");
+    });
+
+    closeMenu.addEventListener("click", () => {
+        menu.classList.remove("open");
+    });
+
+});
+
+
