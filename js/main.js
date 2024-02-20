ab847e15cca74965b47a3a05e5fd7431174fa578
Ivan Gomes <ivanlourencogomes@gmail.com>
2024-02-20 18:51:41 +0000

    contact details section

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@
 
 window.addEventListener("load", (event) => {
 
+    let phoneNumber = "+1 256 222 487";
+    let emailAddress = "herodev@gmail.com";
+    let phone = document.querySelector("#phone-number");
+    let email = document.querySelector("#email-address");
     let menu = document.querySelector("header nav ul.menu");
     let menuIcon = document.querySelector("header nav .menu-icon");
     let closeMenu = document.querySelector("header nav .close-icon");
+    let showContact = document.querySelector("button#show-contact");
+    let contactDetails = document.querySelector("div.contact-details ul");
 
     menuIcon.addEventListener("click", () => {
         menu.classList.add("open");
@@ -12,6 +18,15 @@ window.addEventListener("load", (event) => {
 
     closeMenu.addEventListener("click", () => {
         menu.classList.remove("open");
+    });
+
+    showContact.addEventListener("click", () => {
+        showContact.remove();
+        phone.setAttribute("href", `tel:${phoneNumber}`);
+        phone.innerText = `${phoneNumber}`;
+        email.setAttribute("href", `mailto:${emailAddress}`);
+        email.innerText = `${emailAddress}`;
+        contactDetails.classList.add("show");
     });
 
     document.querySelectorAll('.menu a[href^="#"]').forEach(anchor => {
